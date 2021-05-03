bc1717076c54656b2a27822883ec46a7d02223c7
Manakhov <manakhovsvyatoslav@gmail.com>
2021-05-03 17:30:31 +0300

    lab_1 define main memory

--- lab_1/Src/main.c
+++ lab_1/Src/main.c
@@ -1,7 +1,12 @@
 #include <stdint.h>
 
+#define PERIPH_BASE 0x40000000
+#define PERIPH_BB_BASE (PERIPH_BASE + 0x02000000)
+#define AHB1_BASE (PERIPH_BASE + 0x00020000)
+#define GPIOC_BASE (AHB1_BASE + 0x00000800)
+
 int main(void)
 {
-
+	*((uint32_t *) 0x40023830) |= 1 | 4;
 	for(;;);
 }
