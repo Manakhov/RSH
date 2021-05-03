11a038f69daef755403871cfb262c1794794c719
Manakhov <manakhovsvyatoslav@gmail.com>
2021-05-03 20:23:58 +0300

    lab_1 define GPIO_TypeDef

--- lab_1/Src/main.c
+++ lab_1/Src/main.c
@@ -5,6 +5,23 @@
 #define AHB1_BASE (PERIPH_BASE + 0x00020000)
 #define GPIOC_BASE (AHB1_BASE + 0x00000800)
 
+typedef struct
+{
+	uint32_t MODER;
+	uint32_t OTYPER;
+	uint32_t OSPEEDER;
+	uint32_t PUPDR;
+	uint32_t IDR;
+	uint32_t ODR;
+	uint32_t BSRR;
+	uint32_t LCKR;
+	uint32_t AFRL;
+	uint32_t AFRH;
+} GPIO_TypeDef;
+
+#define GPIOC ((GPIO_TypeDef *) GPIOC_BASE)
+
+
 int main(void)
 {
 	*((uint32_t *) 0x40023830) |= 1 | 4;
