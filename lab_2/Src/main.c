546195fec717bee2f9e90e0a4dffc7a38541bceb
Manakhov <manakhovsvyatoslav@gmail.com>
2021-05-05 22:16:47 +0300

    lab_2 add prescalers

--- lab_2/Src/main.c
+++ lab_2/Src/main.c
@@ -28,8 +28,10 @@ void SysTick_Handler(void)
 
 int main(void)
 {
+	// HSE on
 	RCC->CR |= RCC_CR_HSEON;
 	while((RCC->CR & RCC_CR_HSERDY) == 0);
+	// select HSE
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
 	// PLLM = 4
 	RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLM_5;
@@ -51,10 +53,17 @@ int main(void)
 	// PLLP = 6
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLP_1;
 	RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLP_0;
+	// PLL on
 	RCC->CR |= RCC_CR_PLLON;
 	while((RCC->CR & RCC_CR_PLLRDY) == 0);
-
-	RCC->CR &= ~RCC_CR_HSION;
-	while((RCC->CR & RCC_CR_HSIRDY) == 0);
+	// select PLL
+	RCC->CFGR |= RCC_CFGR_SW & RCC_CFGR_SW_PLL;
+	while((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_PLL);
+	// AHB_Prescaler = 1
+	RCC->CFGR |= RCC_CFGR_HPRE & RCC_CFGR_HPRE_DIV1;
+	// APB1_Prescaler = 4
+	RCC->CFGR |= RCC_CFGR_PPRE1 & RCC_CFGR_PPRE1_DIV4;
+	// APB2_Prescaler = 2
+	RCC->CFGR |= RCC_CFGR_PPRE2 & RCC_CFGR_PPRE2_DIV2;
 
 }
